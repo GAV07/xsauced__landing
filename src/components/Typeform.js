98c3ca01b70c145890e8f52331b78c5f5dead823
Erick <egavin1992@gmail.com>
2020-12-23 19:54:26 -0500

    fix typeform

--- src/components/Typeform.js
+++ src/components/Typeform.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import * as typeformEmbed from '@typeform/embed'
 
 const Typeform = () => {
-    (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
-
-    return (
-        <>
-        <a class="typeform-share button" href="https://form.typeform.com/to/wsWQIHTv?typeform-medium=embed-snippet" data-mode="drawer_right" target="_blank">Launch me </a> 
-        </>
-    )
-}
+    const typeformRef = useRef(null)
+    
+    useEffect(() => {
+      const popup = typeformEmbed.makePopup('https://erickgavin7.typeform.com/to/wsWQIHTv', {
+        mode: 'drawer_right',
+        hideFooter: true,
+        hideHeaders: true,
+        hideScrollbars: true,
+        opacity: 50,
+      })
+      typeformRef.current.addEventListener('click', function() {popup.open()})
+    }, [typeformRef])
+  
+    return <a ref={typeformRef} id="typeform">Join Waitlist</a>
+  }
 
 export default Typeform
